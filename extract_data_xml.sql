37dd7f85fe35600e4f5e7a02bdb3ae84f35cefa4
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2025-01-06 17:41:41 +0100

    update psql procedure

--- extract_data_xml.sql
+++ extract_data_xml.sql
@@ -1,17 +1,38 @@
-CREATE OR REPLACE FUNCTION extract_xml_data()
+-- Procédure pour extraire les données
+
+CREATE OR REPLACE FUNCTION extract_xml_data(xml_content xml)
 RETURNS text AS $$
 DECLARE
     result text;
 BEGIN
-    SELECT array_to_string(xpath('/collection/document/passage/infon[@key="article-id_pmid"]/text()', content), '')
-    || '/' ||
-    array_to_string(xpath('/collection/document/passage/infon[@key=''section_type'' and text()="TITLE"]/../text/text()',
-                          content), '')
-    || ' ' ||
-    array_to_string(xpath('/collection/document/passage/infon[@key=''section_type'' and text()="ABSTRACT"]/../text/text()',
-                          content), '')
-INTO result
-FROM xml_data;
-RETURN result;
+    SELECT array_to_string(xpath('/document/passage/infon[@key="article-id_pmid"]/text()', xml_content), '') ||
+           '/' ||
+           array_to_string(xpath('/document/passage/infon[@key="section_type" and text()="TITLE"]/../text/text()', xml_content), '') ||
+           ' ' ||
+           array_to_string(xpath('/document/passage/infon[@key="section_type" and text()="ABSTRACT"]/../text/text()', xml_content), '')
+    INTO result;
+
+    RETURN result;
 END;
 $$ LANGUAGE plpgsql;
+
+-- Création de la table
+
+DROP TABLE IF EXISTS xml_data;
+CREATE TABLE xml_data(content xml);
+
+-- Pour insérer un fichier dans notre table
+COPY xml_data(content)
+FROM '/tmp/covid2.xml'
+WITH (FORMAT text);
+
+-- Appeler la procédure et envoyer le résultat dans result.txt
+
+COPY (
+    SELECT result
+    FROM (
+        SELECT extract_xml_data(content) AS result
+        FROM xml_data
+    ) subquery
+    WHERE trim(result) != '/'
+) TO '/tmp/result.txt';
