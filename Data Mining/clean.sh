f60c4fa13a52c8fe96599ad5d15d5c9a7c0bf155
Etienne Binginot <etienne.binginot@gmail.com>
2025-01-13 23:20:54 +0100

    feat: script to get mh from pmid

--- Data Mining/clean.sh
+++ Data Mining/clean.sh
@@ -1,42 +1,39 @@
 echo "Récupération des PMID et MESH du fichier"
-sed -e '/MESH:/!d' -r -e 's/^([[:digit:]]*).*MESH:([A-Z][[:digit:]]*).*/\1 \2/' bioconcepts2pubtatorcentral > result
+sed -e '/MESH:/!d' -r -e 's/^([[:digit:]]*).*MESH:([A-Z][[:digit:]]*).*/\1 \2/' bioconcepts2pubtatorcentral > bioconcepts2pubtatorcentral_cleaned
 
 echo "Regroupement des MESH par PMID"
-awk -F' ' '$1==last {printf " %s",$2; next} NR>1 {print "";} {last=$1; printf "%s",$0;} END{print "";}' result > result_final
+awk -F' ' '$1==last {printf " %s",$2; next} NR>1 {print "";} {last=$1; printf "%s",$0;} END{print "";}' bioconcepts2pubtatorcentral_cleaned > bioconcepts2pubtatorcentral_cleaned_grouped
 
 echo "Récupération des noms de MESH"
-awk -F' ' '{print $2}' result_final | sort | uniq > mesh
+awk -F' ' '{print $2}' bioconcepts2pubtatorcentral_cleaned_grouped | sort | uniq > mesh
 
 echo "Nombre de MESH différents : " `wc -l mesh`
 
-echo "Nombre complet d'instances de MESH: " `wc -l result`
+echo "Nombre complet d'instances de MESH: " `wc -l bioconcepts2pubtatorcentral_cleaned`
 
 echo "Récupération du nombre d'instances par MESH, triées par ordre décroissant"
-awk -F' ' '{print $2}' result_final | sort | uniq -c | sort -nr > mesh_count
+awk -F' ' '{print $2}' bioconcepts2pubtatorcentral_cleaned_grouped | sort | uniq -c | sort -nr > mesh_count
 
 echo "Suppression des MESH ayant un support inférieur à 0.1%"
-total_instances=$(wc -l < result_final)
-awk -F' ' '{print $2}' result_final | sort | uniq -c | sort -nr | awk -v total="$total_instances" '$1>0.001*total {print $2}' > mesh_01
+total_instances=$(wc -l < bioconcepts2pubtatorcentral_cleaned_grouped)
+awk -F' ' '{print $2}' bioconcepts2pubtatorcentral_cleaned_grouped | sort | uniq -c | sort -nr | awk -v total="$total_instances" '$1>0.001*total {print $2}' > mesh_support_01
 
-echo "Nombre de MESH gardés : " `wc -l mesh_01`
+echo "Nombre de MESH gardés : " `wc -l mesh_support_01`
 
 echo "Tri des MESH par ordre alphabétique"
-sort mesh_01 > mesh_01_sorted
+sort mesh_support_01 > mesh_support_01_sorted
 
-echo "Suppression des MESH de result_final n'étant pas dans mesh_01"
-awk -F' ' 'NR==FNR{a[$1];next} {out=$1;for (i = 2; i <= NF; i++) {if ($i in a) {out=out" "$i;}};print out}' mesh_01 result_final > result_final_01
+echo "Suppression des MESH de bioconcepts2pubtatorcentral_cleaned_grouped n'étant pas dans mesh_support_01"
+awk -F' ' 'NR==FNR{a[$1];next} {out=$1;for (i = 2; i <= NF; i++) {if ($i in a) {out=out" "$i;}};print out}' mesh_support_01 bioconcepts2pubtatorcentral_cleaned_grouped > bioconcepts2pubtatorcentral_cleaned_grouped_support
 
 echo "Suppression des PMID n'ayant plus de MESH"
-awk -F' ' '{if (NF > 1) print $0}' result_final_01 > pmid_01
+awk -F' ' '{if (NF > 1) print $0}' bioconcepts2pubtatorcentral_cleaned_grouped_support > pmid_01
 
-echo "Tri des MESH par ordre alphabétique dans result_final_01"
-awk -F' ' '{printf "%s", $1; for (i = 2; i <= NF; i++) {mesh[i - 2]=$i}; asort(mesh); for (m in mesh){printf " %s",mesh[m]}; print ""; delete mesh}' pmid_01 > result_final_01_sorted
+echo "Tri des MESH par ordre alphabétique dans bioconcepts2pubtatorcentral_cleaned_grouped_support"
+awk -F' ' '{printf "%s", $1; for (i = 2; i <= NF; i++) {mesh[i - 2]=$i}; asort(mesh); for (m in mesh){printf " %s",mesh[m]}; print ""; delete mesh}' pmid_01 > bioconcepts2pubtatorcentral_cleaned_grouped_support_sorted
 
-echo "Création du fichier final CSV ayant pour entête PMID,MESH1,MESH2,... (en utilisant les MESH de mesh_01). Un 1 dans la colonne signifie que le PMID possède le MESH correspondant."
-echo -n "PMID" > result_final_01.csv
-awk -F' ' '{printf ",%s", $1}' mesh_01_sorted >> result_final_01.csv
-echo "" >> result_final_01.csv
-awk -F' ' 'NR==FNR{meshs[NR]=$1;next} {printf "%s", $1;current_field=2; for(mesh in meshs) {if ($current_field == meshs[mesh]) {printf ",1"; current_field=current_field + 1} else {printf ",?"}};print ""}' mesh_01_sorted result_final_01_sorted >> result_final_01.csv
-
-echo "Suppression des fichiers temporaires"
-rm result result_final mesh mesh_count mesh_01 mesh_01_sorted result_final_01 pmid_01 result_final_01_sorted
+echo "Création du fichier final CSV ayant pour entête PMID,MESH1,MESH2,... (en utilisant les MESH de mesh_support_01). Un 1 dans la colonne signifie que le PMID possède le MESH correspondant."
+echo -n "PMID" > bioconcepts2pubtatorcentral_cleaned.csv
+awk -F' ' '{printf ",%s", $1}' mesh_support_01_sorted >> bioconcepts2pubtatorcentral_cleaned.csv
+echo "" >> bioconcepts2pubtatorcentral_cleaned.csv
+awk -F' ' 'NR==FNR{meshs[NR]=$1;next} {printf "%s", $1;current_field=2; for(mesh in meshs) {if ($current_field == meshs[mesh]) {printf ",1"; current_field=current_field + 1} else {printf ",?"}};print ""}' mesh_support_01_sorted bioconcepts2pubtatorcentral_cleaned_grouped_support_sorted >> bioconcepts2pubtatorcentral_cleaned.csv
---
+++ Data Mining/mh_from_pmid.sh
@@ -0,0 +1,35 @@
+echo "Récupération des PMID du fichier"
+awk -F' ' '{print $1}' bioconcepts2pubtatorcentral_cleaned_grouped_support | uniq > pmid
+
+echo "Nombre de PMID différents : " `wc -l pmid`
+
+#echo "Récupération des MH de chaque PMID"
+#while read pmid; do
+#  echo "$pmid " $(curl -s https://pubmed.ncbi.nlm.nih.gov/$pmid/?format=pubmed | sed -e '/MH /!d' -e 's/MH  - //g' -e 's/\/.*//g' | tr -d "\r")
+#done < pmid > pmid_mh
+
+echo "Récupération des MH de chaque PMID en utilisant le FTP"
+for i in {1..1274}; do
+    echo "Téléchargement du fichier $i"
+    number=$(printf "%04d" $i)
+    wget -q https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed25n$number.xml.gz
+    gzip -d pubmed25n$number.xml.gz
+
+    echo "Extraction des MH du fichier $i"
+    awk '
+    BEGIN { RS="</PubmedArticle>"; FS="\n" }
+    /<PMID Version="1">[0-9]+<\/PMID>/ {
+        pmid=gensub(/.*<PMID Version="1">([0-9]+)<\/PMID>.*/,"\\1","g",$0)
+        mesh=$0
+        sub(/.*<MeshHeadingList>/,"",mesh)
+        sub(/<\/MeshHeadingList>.*/,"",mesh)
+        mesh=gensub(/<MeshHeading>|<\/MeshHeading>/,"","g",mesh)
+        mesh=gensub(/<DescriptorName[^>]*>([^<]+)<\/DescriptorName>/,"\"\\1\"","g",mesh)
+        mesh=gensub(/<QualifierName[^>]*>[^<]*<\/QualifierName>/,"","g",mesh)
+        gsub(/\s+/," ",mesh)
+        if (pmid && mesh) print pmid" "mesh
+    }
+    ' pubmed25n$number.xml >> pmid_mh_ftp
+
+    rm pubmed25n$number.xml
+done
