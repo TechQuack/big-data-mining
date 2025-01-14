b1716cd15da84712e92d4f3bfd127eed093c305e
Etienne Binginot <etienne.binginot@gmail.com>
2025-01-14 22:19:12 +0100

    feat: remove empty lines

--- Data Mining/text_from_extraction.sh
+++ Data Mining/text_from_extraction.sh
@@ -3,3 +3,6 @@ sed -E 's/[[:digit:]]+\///' result.txt > pmid_text.txt
 
 echo "Mise en forme du texte en mettant chaque phrase sur une nouvelle ligne"
 sed -E 's/\./\.\n/g' pmid_text.txt > pmid_text_sentences.txt
+
+echo "Suppression des lignes vides et espaces en début de ligne"
+sed -E -e 's/\r//g' -e '/^$/d' -e 's/^[[:space:]]+//' pmid_text_sentences.txt > pmid_text_sentences_cleaned.txt
