bccc313441e34b939cfa4f8b0ef79ef8bec5d229
Etienne Binginot <etienne.binginot@gmail.com>
2025-01-15 09:17:55 +0100

    feat: clean up result file

--- Data Mining/text_from_extraction.sh
+++ Data Mining/text_from_extraction.sh
@@ -1,8 +1,11 @@
+echo "Suppression des retours chariots"
+sed -E -e 's/\r//g'  result.txt > result_cleaned.txt
+
 echo "Récupération du texte de chaque PMID"
-sed -E 's/[[:digit:]]+\///' result.txt > pmid_text.txt
+sed -E 's/[[:digit:]]+\///' result_cleaned.txt > pmid_text.txt
 
 echo "Mise en forme du texte en mettant chaque phrase sur une nouvelle ligne"
 sed -E 's/\./\.\n/g' pmid_text.txt > pmid_text_sentences.txt
 
 echo "Suppression des lignes vides et espaces en début de ligne"
-sed -E -e 's/\r//g' -e '/^$/d' -e 's/^[[:space:]]+//' pmid_text_sentences.txt > pmid_text_sentences_cleaned.txt
+sed -E -e '/^$/d' -e 's/^[[:space:]]+//' pmid_text_sentences.txt > pmid_text_sentences_cleaned.txt
